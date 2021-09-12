d8d8eab7eeb86cd7ed2b3ed1ad823261c2456fcb
Manoj Pawar <vidnyanhub@gmail.com>
2021-09-12 22:59:47 +0530

    Update proto_code_gen.sh

--- proto_code_gen.sh
+++ proto_code_gen.sh
@@ -3,3 +3,4 @@ export GOBIN=$GOPATH/bin
 export PATH=$PATH:$GOROOT:$GOPATH:$GOBIN
 protoc -I=src/ --go_out=src/ src/simple_example/simple_message.proto
 protoc -I=src/ --go_out=src/ src/enum_example/enum_message.proto
+protoc -I=src/ --go_out=src/ src/comment_example/comments.proto
