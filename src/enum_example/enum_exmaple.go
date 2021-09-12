cbb1f41896cc0d9e9dee0d72e2ee7505f8f9b9e3
Manoj Pawar <vidnyanhub@gmail.com>
2021-09-12 22:30:45 +0530

    Update enum_exmaple.go

--- src/enum_example/enum_exmaple.go
+++ src/enum_example/enum_exmaple.go
@@ -1,3 +1,13 @@
 package enum_example
 
-func NewEnumMessage() {}
+import (
+	"log"
+
+	"github.com/manojpawar94/grpc/src/enum_example/enumpb"
+)
+
+func NewEnumMessage() *enumpb.EnumMessage {
+	em := &enumpb.EnumMessage{Id: 1234, DayOfWeek: enumpb.Day_MONDAY}
+	log.Printf("The enum message is :%v\n", em)
+	return em
+}
