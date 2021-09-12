551065ce5c7bdab715c2cd533c7b60216a6017bf
Manoj Pawar <vidnyanhub@gmail.com>
2021-09-12 22:09:32 +0530

    enum example with protocol buffer

--- proto_code_gen.sh
+++ proto_code_gen.sh
@@ -2,3 +2,4 @@ export GOPATH=$HOME/go
 export GOBIN=$GOPATH/bin
 export PATH=$PATH:$GOROOT:$GOPATH:$GOBIN
 protoc -I=src/ --go_out=src/ src/sample/sample.proto
+protoc -I=src/ --go_out=src/ src/enum_example/enum_message.proto
